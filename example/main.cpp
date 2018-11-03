fd5be4e919d256c243396db6a72fe9c9998c5cce
Takayama Fumihiko <tekezo@pqrs.org>
2018-11-03 11:09:30 +0900

    add comments

--- example/main.cpp
+++ example/main.cpp
@@ -5,16 +5,22 @@
 
 int main(void) {
   int i = 0;
+
+  // Create `pqrs::thread_wait`.
   auto w = pqrs::make_thread_wait();
 
+  // Note: You must capture `pqrs::thread_wait` by copy.
+  // (Do not capture by reference like [&w].)
   auto thread = std::thread([&i, w] {
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
     i = 100;
+
     w->notify();
   });
 
   std::cout << i << std::endl; // 0
 
+  // Wait until another thread calls `pqrs::thread_wait::notify`.
   w->wait_notice();
 
   std::cout << i << std::endl; // 500
