3797f071e2a0a1636d4b4fa7254d16a80b4b296f
Takayama Fumihiko <tekezo@pqrs.org>
2018-11-03 11:34:06 +0900

    version 1.0.0

--- include/pqrs/thread_wait.hpp
+++ include/pqrs/thread_wait.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+// pqrs::thread_wait v1.0.0
+
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
 // (See http://www.boost.org/LICENSE_1_0.txt)
