5d011dd54c1192a86299008ea09ce0fdf92299da
Takayama Fumihiko <tekezo@pqrs.org>
2018-11-03 10:48:19 +0900

    add tests

---
+++ tests/src/test.cpp
@@ -0,0 +1,35 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include <chrono>
+#include <pqrs/thread_wait.hpp>
+#include <thread>
+
+TEST_CASE("thread_wait") {
+  {
+    int count = 0;
+    auto w = pqrs::make_thread_wait();
+    auto t = std::thread([&count, w] {
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+      ++count;
+      w->notify();
+    });
+    REQUIRE(count == 0);
+
+    w->wait_notice();
+    REQUIRE(count == 1);
+
+    t.join();
+  }
+}
+
+TEST_CASE("stress testing") {
+  for (int i = 0; i < 10000; ++i) {
+    auto w = pqrs::make_thread_wait();
+    auto t = std::thread([w] {
+      w->notify();
+    });
+    w->wait_notice();
+    t.join();
+  }
+}
