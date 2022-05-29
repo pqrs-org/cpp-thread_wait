263b2dc2f6d64b1d88e09d7db3e08d4ca3100253
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-29 10:23:38 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,35 +1,40 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <chrono>
 #include <pqrs/thread_wait.hpp>
 #include <thread>
 
-TEST_CASE("thread_wait") {
-  {
-    int count = 0;
-    auto w = pqrs::make_thread_wait();
-    auto t = std::thread([&count, w] {
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
-      ++count;
-      w->notify();
-    });
-    REQUIRE(count == 0);
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
 
-    w->wait_notice();
-    REQUIRE(count == 1);
+  "thread_wait"_test = [] {
+    {
+      int count = 0;
+      auto w = pqrs::make_thread_wait();
+      auto t = std::thread([&count, w] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        ++count;
+        w->notify();
+      });
+      expect(count == 0);
 
-    t.join();
-  }
-}
+      w->wait_notice();
+      expect(count == 1);
 
-TEST_CASE("stress testing") {
-  for (int i = 0; i < 10000; ++i) {
-    auto w = pqrs::make_thread_wait();
-    auto t = std::thread([w] {
-      w->notify();
-    });
-    t.detach();
-    w->wait_notice();
-  }
+      t.join();
+    }
+  };
+
+  "stress testing"_test = [] {
+    for (int i = 0; i < 10000; ++i) {
+      auto w = pqrs::make_thread_wait();
+      auto t = std::thread([w] {
+        w->notify();
+      });
+      t.detach();
+      w->wait_notice();
+    }
+  };
+
+  return 0;
 }
